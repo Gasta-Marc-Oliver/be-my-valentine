4c01cbcfc98177eef926588770df134b67cf1ac0
Gasta Marc Oliver <marcolivergastagonzales@gmail.com>
2025-01-28 02:46:25 +0800

    valentine-gasta

---
+++ script.js
@@ -0,0 +1,22 @@
+function goToNextPage() {
+    window.location.href = 'next.html';
+}
+
+function handleYes() {
+    const card = document.querySelector('.card');
+    card.innerHTML = '<h1>Thank you, Ms. Angelica.</h1>';
+}
+
+function handleNo() {
+    const question = document.getElementById('question');
+    const options = document.getElementById('options');
+
+    if (question.textContent === 'Would you like to be my Valentine?') {
+        question.textContent = 'Are you sure?';
+    } else if (question.textContent === 'Are you sure?') {
+        question.textContent = 'Are you really sure?';
+    } else {
+        question.textContent = 'Would you like to be my Valentine?';
+        options.innerHTML = '<button onclick="handleYes()">Yes</button> <button onclick="handleNo()">No</button>';
+    }
+}
